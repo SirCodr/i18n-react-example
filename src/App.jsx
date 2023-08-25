b2dfbe33db0cfaa9b076125b75bb6507f04f34b6
SirCodr <jotadeveloper00@gmail.com>
2023-08-24 23:39:06 -0500

    Add basic implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useTranslation } from 'react-i18next'
+import langs from './consts/langs'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { t, i18n } = useTranslation('welcome')
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header className='flex justify-center w-full gap-x-2'>
+        {Object.keys(langs).map((lng) => (
+          <button
+            key={lng}
+            style={{
+              fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal'
+            }}
+            type='submit'
+            onClick={() => i18n.changeLanguage(lng)}
+          >
+            {langs[lng].nativeName}
+          </button>
+        ))}
+      </header>
+      <div className='card'>
+        <p>{t('title')}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
